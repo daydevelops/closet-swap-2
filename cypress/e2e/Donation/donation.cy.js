c3bf9c234a50f864739e77a2b787c64ae4421a22
mona day <monaday@daydevelops.com>
2024-12-28 11:35:41 -0500

    adding descriptions to each cypress test

--- cypress/e2e/Donation/donation.cy.js
+++ cypress/e2e/Donation/donation.cy.js
@@ -1,8 +1,14 @@
 
 describe('Testing Donations Page', () => {
-    it('allows a user to donate', () => {
+    it('allows a guest to donate', () => {
+        // given I am a guest
+        // when I visit the donations page
+        // then I should see a form to donate
     });
 
-    it('allows a guest to donate', () => {
+    it('allows a user to donate', () => {
+        // given I am an authenticated user
+        // when I visit the donations page
+        // then I should see a form to donate
     });
 });
